5bcf2c236c7b20163cbb598463b1f30abec4a1b9
Mond45 <mondc45@gmail.com>
2022-09-16 15:58:59 +0700

    fixed erase many

--- erase_many/student.h
+++ erase_many/student.h
@@ -4,43 +4,43 @@
 template <typename T>
 void CP::vector<T>::erase_many(const std::vector<int> &pos)
 {
-    // T *newData = new T[mCap];
-    // T *p = newData;
-    // size_t ip = 0;
-    // for (size_t i = 0; i < mSize; ++i)
-    // {
-    //     if (i == pos[ip])
-    //     {
-    //         ++ip;
-    //     }
-    //     else
-    //     {
-    //         *(p++) = mData[i];
-    //     }
-    // }
-    // delete[] mData;
-    // mSize -= pos.size();
-    // mData = newData;
-
-    int i = 0;
-    T *p = mData;
-    while (i < pos[0])
+    T *newData = new T[mCap];
+    T *p = newData;
+    size_t ip = 0;
+    for (size_t i = 0; i < mSize; ++i)
     {
-        *(p++) = mData[i];
-        ++i;
+        if (ip < pos.size() && i == pos[ip])
+        {
+            ++ip;
+        }
+        else
+        {
+            *(p++) = mData[i];
+        }
     }
-    for (int i = 0; i < pos.size() - 1; ++i)
-    {
-        for (int j = pos[i] + 1; j < pos[i + 1]; ++j)
-            *(p++) = mData[j];
-    }
-    i = *(pos.end() - 1) + 1;
-    while (i < mSize)
-    {
-        *(p++) = mData[i];
-        ++i;
-    }
+    delete[] mData;
     mSize -= pos.size();
+    mData = newData;
+
+    // int i = 0;
+    // T *p = mData;
+    // while (i < pos[0])
+    // {
+    //     *(p++) = mData[i];
+    //     ++i;
+    // }
+    // for (int i = 0; i < pos.size() - 1; ++i)
+    // {
+    //     for (int j = pos[i] + 1; j < pos[i + 1]; ++j)
+    //         *(p++) = mData[j];
+    // }
+    // i = *(pos.end() - 1) + 1;
+    // while (i < mSize)
+    // {
+    //     *(p++) = mData[i];
+    //     ++i;
+    // }
+    // mSize -= pos.size();
 }
 
 #endif
