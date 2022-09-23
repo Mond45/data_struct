181d45f98ca9103edb38d2d43cab8c04505264a6
Mond45 <mondc45@gmail.com>
2022-09-23 16:26:17 +0700

    queue check

--- queue_check.cpp
+++ queue_check.cpp
@@ -4,67 +4,48 @@ using namespace std;
 
 int main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(nullptr);
-    freopen("output/output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n;
     cin >> n;
     while (n--)
     {
-        int mFront, mSize, mCap, last, correction;
-        cin >> mFront >> mSize >> mCap >> last >> correction;
-        switch (correction)
+        int mFront, mSize, mCap, last, operation;
+        cin >> mFront >> mSize >> mCap >> last >> operation;
+        if (last < mCap && mFront < mCap && mSize <= mCap && (mFront + mSize) % mCap == last)
         {
-        case 0:
+            cout << "OK\n";
+        }
+        else
         {
-            if (mFront >= mCap || mSize > mCap || last > mCap || mFront + mSize != last || (mFront == last && mSize != mCap))
+            switch (operation)
+            {
+            case 0:
+            {
                 cout << "WRONG\n";
-            else
-                cout << "OK\n";
-            break;
-        }
-        case 1:
-        {
-            if (mSize == mCap && mFront != last)
-                cout << "WRONG " << last << '\n';
-            else if (mFront + mSize != last)
-                cout << "WRONG " << last - mSize << '\n';
-            else
-                cout << "OK\n";
-            break;
-        }
-        case 2:
-        {
-            if (mFront == last && mSize != mCap)
-                cout << "WRONG " << mCap << '\n';
-            else if (mFront + mSize != last)
-                cout << "WRONG " << last - mFront << '\n';
-            else
-                cout << "OK\n";
-            break;
-        }
-        case 3:
-        {
-            if (mFront == last && mCap != mSize)
-                cout << "WRONG " << mSize << '\n';
-
-            else if (mCap < last || mCap < mFront || mCap < mSize)
-                cout << "WRONG " << max(max(last, mFront) + 1, mSize) << '\n';
-
-            else
-                cout << "OK\n";
-            break;
-        }
-        case 4:
-        {
-            if (mSize == mCap && last != mFront)
-                cout << "WRONG " << mFront << '\n';
-            else if (mFront + mSize != last)
-                cout << "WRONG " << mFront + mSize << '\n';
-            else
-                cout << "OK\n";
-            break;
-        }
+                break;
+            }
+            case 1:
+            {
+                cout << "WRONG " << (last + mCap - mSize) % mCap << '\n';
+                break;
+            }
+            case 2:
+            {
+                cout << "WRONG " << (last - mFront + mCap) % mCap << '\n';
+                break;
+            }
+            case 3:
+            {
+                cout << "WRONG " << (mFront + mSize == last ? last + 1 : mFront - last + mSize) << '\n';
+                break;
+            }
+            case 4:
+            {
+                cout << "WRONG " << (mFront + mSize) % mCap << '\n';
+                break;
+            }
+            }
         }
     }
     return 0;
